a3453ede6d1fb6b53fc3b16d716d0706bed558d5
Lucho00Cuba <lomv0209@gmail.com>
2023-11-05 22:28:45 +0100

    pass 8nd stage

--- app/main.py
+++ app/main.py
@@ -9,13 +9,13 @@ PORT = 4221
 BUFFER = 1024
 CRLF = "\r\n"
 
-def requests_path(data):
+def getPath(data):
     decoded_data = data.decode()
     first_line = decoded_data.split("\r\n")[0]
     path = first_line.split(" ")[1]
     return path
 
-def requests_userAgent(data):
+def getHeaderUserAgent(data):
     decoded_data = data.decode()
     first_line = decoded_data.split("\r\n")
     for header in first_line:
@@ -24,6 +24,10 @@ def requests_userAgent(data):
 
 def make_response(line):
     return line.encode()
+
+def getBody(data: str):
+    decoded_data = data[data.find(b"\r\n\r\n") + 4 :]
+    return decoded_data.decode()
 
 def getFileContent(directory_path, file_path):
     whole_path = Path(directory_path).joinpath(Path(file_path))
@@ -34,46 +38,70 @@ def getFileContent(directory_path, file_path):
         return True, file_contents
     return False, ""
 
-def handler_req(client_socket,client_addr, directory_path):
-    try:
-        with client_socket:
-            print(f"Connected to {client_addr}")
-            data = client_socket.recv(BUFFER)
-            path = requests_path(data)
-            if path == '/':
-                client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF + CRLF}"))
-            elif path.startswith('/echo/'):
-                message = path.split("/echo/")[1]
-                client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
-                client_socket.send(make_response(f"Content-Type: text/plain{CRLF}"))
-                client_socket.send(make_response(f"Content-Length: {len(message)} {CRLF}"))
-                client_socket.send(make_response(f"{CRLF}"))
-                client_socket.send(make_response(f"{message} {CRLF + CRLF}"))
-            elif path == '/user-agent':
-                message = requests_userAgent(data)
-                client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
-                client_socket.send(make_response(f"Content-Type: text/plain{CRLF}"))
-                client_socket.send(make_response(f"Content-Length: {len(message)} {CRLF}"))
-                client_socket.send(make_response(f"{CRLF}"))
-                client_socket.send(make_response(f"{message} {CRLF + CRLF}"))
-            elif path.startswith('/files/'):
-                file_path = path.split("/files/")[1]
-                file_exists, content = getFileContent(directory_path, file_path)
-                if file_exists:
-                    client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
-                    client_socket.send(make_response(f"Content-Type: application/octet-stream {CRLF}"))
-                    client_socket.send(make_response(f"Content-Length: {len(content)} {CRLF}"))
-                    client_socket.send(make_response(f"{CRLF}"))
-                    client_socket.send(make_response(f"{content} {CRLF + CRLF}"))
-                else:
-                    client_socket.send(make_response(f"HTTP/1.1 404 Not Found {CRLF + CRLF}"))
-            else:
-                client_socket.send(make_response(f"HTTP/1.1 404 Not Found {CRLF + CRLF}"))
-            print(f"requests {path}")
-    except ConnectionError:
-        pass # nc probe
+def writeToFile(directory_path, file_path: str, content):
+    whole_path = Path(directory_path).joinpath(Path(file_path))
+    if Path(directory_path).exists() and bool(file_path.strip()):
+        with open(whole_path, "w") as file:
+            file.write(content)
+            file.close()
+        return True
+    return False
 
+def handle_POST_request(data: str, sock: socket.socket, directory_path: str):
+    path = getPath(data)
+    if path.startswith("/files/"):
+        body = getBody(data)
+        file_path = path.split("/files/")[1]
+        success = writeToFile(directory_path, file_path, body)
+        if success:
+            sock.send(b"HTTP/1.1 201 Created\r\n\r\n")
+        else:
+            sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    else:
+        sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    sock.close()
+
+def handle_GET_request(data, client_socket, directory_path):
+    path = getPath(data)
+    if path == '/':
+        client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF + CRLF}"))
+    elif path.startswith('/echo/'):
+        message = path.split("/echo/")[1]
+        client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
+        client_socket.send(make_response(f"Content-Type: text/plain{CRLF}"))
+        client_socket.send(make_response(f"Content-Length: {len(message)} {CRLF}"))
+        client_socket.send(make_response(f"{CRLF}"))
+        client_socket.send(make_response(f"{message} {CRLF + CRLF}"))
+    elif path == '/user-agent':
+        message = getHeaderUserAgent(data)
+        client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
+        client_socket.send(make_response(f"Content-Type: text/plain{CRLF}"))
+        client_socket.send(make_response(f"Content-Length: {len(message)} {CRLF}"))
+        client_socket.send(make_response(f"{CRLF}"))
+        client_socket.send(make_response(f"{message} {CRLF + CRLF}"))
+    elif path.startswith('/files/'):
+        file_path = path.split("/files/")[1]
+        file_exists, content = getFileContent(directory_path, file_path)
+        if file_exists:
+            client_socket.send(make_response(f"HTTP/1.1 200 OK {CRLF}"))
+            client_socket.send(make_response(f"Content-Type: application/octet-stream {CRLF}"))
+            client_socket.send(make_response(f"Content-Length: {len(content)} {CRLF}"))
+            client_socket.send(make_response(f"{CRLF}"))
+            client_socket.send(make_response(f"{content} {CRLF + CRLF}"))
+        else:
+            client_socket.send(make_response(f"HTTP/1.1 404 Not Found {CRLF + CRLF}"))
+    else:
+        client_socket.send(make_response(f"HTTP/1.1 404 Not Found {CRLF + CRLF}"))
+    print(f"requests {path}")
     client_socket.close()
+
+def handle_request(client_socket, client_addr, directory_path):
+    print(f"Connected to {client_addr}")
+    data = client_socket.recv(1024)
+    if "GET" in data.decode():
+        handle_GET_request(data, client_socket, directory_path)
+    if "POST" in data.decode():
+        handle_POST_request(data, client_socket, directory_path)
 
 def main():
 
@@ -90,7 +118,7 @@ def main():
     #socket.create_connection(("localhost", PORT), timeout=5)
     while True:
         sock, addr = server_socket.accept()  # wait for client
-        threading.Thread(target=handler_req, args=(sock,addr,directory_path,)).start()
+        threading.Thread(target=handle_request, args=(sock,addr,directory_path,)).start()
 
 if __name__ == "__main__":
     main()
